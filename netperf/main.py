0b42c11baa752f3bc20b1bc829c386f7d4cb082a
Phantomn <tmdvyr123@gmail.com>
2024-08-26 03:19:44 +0900

    fix netperf

--- netperf/main.py
+++ netperf/main.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     parser.add_argument("arch", help="Architecture of the receiver")
     parser.add_argument("sender_ssh_pw", help="SSH password for the sender.")
     parser.add_argument("receiver_ip", help="IP address of the receiver.")
+    parser.add_argument("test_type", help="Performing Test Type")
     parser.add_argument("receiver_ssh_id", help="SSH username for the receiver.")
     parser.add_argument("receiver_ssh_pw", help="SSH password for the receiver.")
-    parser.add_argument("test", help="Name of the Scapy script to run.")
 
     args = parser.parse_args()
-    stage = Stage(args.arch, args.sender_ssh_pw, args.receiver_ip, args.receiver_ssh_id, args.receiver_ssh_pw, args.test)
+    stage = Stage(args.arch, args.sender_ssh_pw, args.receiver_ip, args.test_type, args.receiver_ssh_id, args.receiver_ssh_pw)
     stage.run()
